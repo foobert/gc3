bf14abbd50e0bb875591ed6f93a3e7935edc7e8f
foobert <0x002a@gmail.com>
2018-01-12 10:59:47 +0100

    Switch back to non-concurrent update

--- lookup/lib/update.js
+++ lookup/lib/update.js
@@ -1,7 +1,7 @@
 const async = require("async");
 const debug = require("debug")("gc-lookup");
 
-async function update(collection, query, mapper, concurrency = 1) {
+async function updateFancy(collection, query, mapper, concurrency = 1) {
   const q = async.queue(async (doc, done) => {
     if (doc == null) {
       done();
@@ -28,6 +28,23 @@ async function update(collection, query, mapper, concurrency = 1) {
   await new Promise((resolve, reject) => {
     q.push(null, resolve);
   });
+}
+
+async function update(collection, query, mapper) {
+  const cursor = await collection.find(query);
+  while (await cursor.hasNext()) {
+    let doc = await cursor.next();
+    try {
+      let update = await Promise.resolve(mapper(doc));
+      await collection.update(
+        { _id: doc._id },
+        { $set: update },
+        { upsert: true }
+      );
+    } catch (err) {
+      debug("Error while updating %s", doc._id, err);
+    }
+  }
 }
 
 module.exports = update;
